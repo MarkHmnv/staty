6b1c12b657042ce7933659aa6c101c1c5f3ca976
Mark <42696773+MarkHmnv@users.noreply.github.com>
2023-08-07 18:50:13 +0300

    Feat confidence interval (#2)
    * Add confidence interval and score calculations in staty
    
    Extended 'staty.py' module to include z/t score calculations and confidence interval computations for single or paired data, assuming equal or unknown variances.
    
    * Add new statistical tests and improve error handling
    
    Added tests for the pooled variance, z-score, tscore, z-interval, z-interval with equal variances, t-interval, and t-interval with equal variances functions.

--- test/staty_test.py
+++ test/staty_test.py
@@ -14,6 +14,11 @@ class StatyTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             staty.var([2])
 
+    def test_pooled_var(self):
+        self.assertEqual(staty.pooled_var([2, 4, 8, 16], [6, 8, 12, 24]), 51.666666666666664)
+        with self.assertRaises(ValueError):
+            staty.pooled_var([2], [2])
+
     def test_stdev(self):
         self.assertAlmostEqual(staty.stdev([2, 4, 6, 8]), 2.581988897471611)
         with self.assertRaises(ValueError):
@@ -54,9 +59,43 @@ class StatyTest(unittest.TestCase):
 
     def test_zscore(self):
         self.assertEqual(staty.zscore([1, 2, 3, 4]),
+                         [-1.3416407864998738, -0.4472135954999579, 0.4472135954999579, 1.3416407864998738])
+        with self.assertRaises(ValueError):
+            staty.zscore([2])
+
+    def test_tscore(self):
+        self.assertEqual(staty.tscore([1, 2, 3, 4]),
                          [-1.161895003862225, -0.3872983346207417, 0.3872983346207417, 1.161895003862225])
         with self.assertRaises(ValueError):
-            staty.zscore([1])
+            staty.tscore([2])
+
+    def test_z_interval(self):
+        lower, upper = staty.z_interval([2, 4, 6, 8])
+        self.assertAlmostEqual(lower, 2.808693648558546)
+        self.assertAlmostEqual(upper, 7.191306351441455)
+        with self.assertRaises(ValueError):
+            staty.z_interval([2])
+
+    def test_z_interval_equal_var(self):
+        lower, upper = staty.z_interval_equal_var([2, 4, 6, 8], [3, 5, 7, 9])
+        self.assertAlmostEqual(lower, -4.098975161522808)
+        self.assertAlmostEqual(upper, 2.098975161522808)
+        with self.assertRaises(ValueError):
+            staty.z_interval_equal_var([2], [2])
+
+    def test_t_interval(self):
+        lower, upper = staty.t_interval([2, 4, 6, 8])
+        self.assertAlmostEqual(lower, 0.891479486478242)
+        self.assertAlmostEqual(upper, 9.108520513521757)
+        with self.assertRaises(ValueError):
+            staty.t_interval([2])
+
+    def test_t_interval_equal_var(self):
+        lower, upper = staty.t_interval_equal_var([2, 4, 6, 8], [3, 5, 7, 9])
+        self.assertAlmostEqual(lower, -5.467429386032912)
+        self.assertAlmostEqual(upper, 3.4674293860329124)
+        with self.assertRaises(ValueError):
+            staty.t_interval_equal_var([2], [2])
 
 
 if __name__ == '__main__':
